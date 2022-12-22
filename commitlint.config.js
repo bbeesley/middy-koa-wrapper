a8db8872c180a4174d36dafc0ff50c8637ae68d9
Bill Beesley <bbeesley@me.com>
2022-12-22 14:09:33 +0000

    ci: migrate to github actions

--- commitlint.config.js
+++
@@ -1,5 +0,0 @@
-const { types } = require('conventional-commit-types');
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: { 'type-enum': [2, 'always', Object.keys(types)] },
-};
